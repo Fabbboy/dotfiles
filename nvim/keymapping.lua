84cb4ca2fc24e9cc6e29c6d30023ab22f238a76e
Fabboy <schaubfabrice@gmail.com>
2024-02-28 12:33:33 +0100

    added formating

--- nvim/keymapping.lua
+++ nvim/keymapping.lua
@@ -10,7 +10,7 @@ vim.api.nvim_set_keymap('n', '<C-TAB>', ':NvimTreeClose<CR>', { noremap = true, 
 vim.api.nvim_set_keymap('n', '<C-t>', ':belowright split<CR>:term<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-q>', ':q<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('t', '<C-q>', '<C-\\><C-n>:q<CR>', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap('n', '<A-S-f>', ':lua vim.lsp.buf.format({ async = true })<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<S-Down>', ':m .+1<CR>==', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<S-Up>', ':m .-2<CR>==', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<S-Down>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
